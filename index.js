632d8a1ad3de586e50d43b929238e4e5f8d00736
SofiaFreitas711 <anasofiafreitas711@gmail.com>
2024-04-15 22:09:47 +0100

    Not zoomed anymore

--- index.js
+++ index.js
@@ -4,9 +4,9 @@ let w = window.innerWidth
 let h = window.innerHeight
 
 if(w > 1000){
-    document.querySelector("body").style = `margin: ${h*0.4}px; overflow: hidden; width: ${w* 3/5}px; height: ${h*4/5}px;`
+    document.querySelector("body").style = `margin: ${h}px; overflow: hidden; width: ${w* 3/5}px; height: ${h*4/5}px;`
 }else{
-    document.querySelector("body").style = `margin-top: ${h*0.8}px; overflow: hidden; width: 1500px; height: 1500px;`
+    document.querySelector("body").style = `margin-top: 0px; overflow: hidden; width: ${w*1.5}px; height: ${h}px;`
 }
 
 async function getFact(){
@@ -16,6 +16,7 @@ async function getFact(){
     fact = data.text
     
     document.querySelector("#fact").setAttribute("value", fact)
+        
 }
 
 document.body.onkeyup = function(e){
@@ -25,7 +26,17 @@ document.body.onkeyup = function(e){
 }
 
 document.body.addEventListener('touchstart', function(e){
-    getFact()
+    
+    getFact()  
+    document.querySelector("#fact").setAttribute("animation",{
+        property: "opacity",
+        dir: "alternate",
+        to: 1,
+        dur: 3000,
+        easing: "easeInOutSine",
+        loop: false
+    })
+    console.log(fact);
 })
 
 // setInterval(function(){
