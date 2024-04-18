84e5e0091f07de6d2b37b9f1b879baf6e564a3e7
SofiaFreitas711 <anasofiafreitas711@gmail.com>
2024-04-18 13:31:27 +0100

    done

--- index.js
+++ index.js
@@ -10,12 +10,8 @@ async function getFact(){
     const data = await response.json()
 
     fact = data.text
-    factTxt.setAttribute("value", fact)
-    
-    // if(fact.length < 150){
-    //     document.querySelector("#fact").setAttribute("value", fact)
-    // }
-         
+    factTxt.setAttribute("value", fact.toUpperCase())
+             
 }
 
 document.body.onkeyup = function(e){
@@ -25,30 +21,5 @@ document.body.onkeyup = function(e){
 }
 
 document.body.addEventListener('touchstart', function(e){
-    
     getFact()  
-    
-    // document.querySelector("#fact").setAttribute("animation",{
-    //     property: "opacity",
-    //     dir: "alternate",
-    //     to: 1,
-    //     dur: 3000,
-    //     easing: "easeInOutSine",
-    //     loop: false
-    // })
-    // console.log(fact);
 })
-
-console.log(document.querySelector("#fact").getAttribute("opacity"));
-
-// else if(document.querySelector("#fact").getAttribute("opacity")>0){
-//     opacity = document.querySelector("#fact").getAttribute("opacity") + 0.1
-//     document.querySelector("#fact").setAttribute("opacity", opacity)
-// }
-// setInterval(function(){
-//     let pZ = document.querySelector("a-camera").object3D.position.z - 5
-//     document.querySelector("#fact").setAttribute("position", `0 0.3 ${pZ}`)
-//     // console.log(document.querySelector("a-camera").object3D);
-    
-// },10)
-
